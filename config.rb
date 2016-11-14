419c20c5ea985d6c8ecd4347ab678dfd0a459075
Guayo Mena <guayo.mena@gmail.com>
2016-11-13 18:17:08 -0600

    Installed dato correctly

--- config.rb
+++ config.rb
@@ -1,6 +1,8 @@
 ###
 # Page options, layouts, aliases and proxies
 ###
+set :site_url, "https://ngcentralamerica.com"
+
 
 # Per-page layout changes:
 #
@@ -22,13 +24,6 @@ end
 
 activate :directory_indexes
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
 # General configuration
 
 # Reload the browser automatically whenever files change
@@ -45,6 +40,7 @@ helpers do
   def nav_active(path)
     current_page.path == path ? {:class => "active"} : {}
   end
+
   # Returns all pages under a certain directory.
   def sub_pages(dir)
     sitemap.resources.select do |resource|
@@ -53,6 +49,7 @@ helpers do
       end
     end.sort_by { |resource| resource.data.weight }
   end
+
   # Returns all pages under a certain directory.
   def sub_pages_by_date(dir)
     sitemap.resources.select do |resource|
@@ -63,13 +60,13 @@ helpers do
   end
 end
 
+activate :dato,
+  domain: 'https://ngcentralamerica.com',
+  token: '93430d102176ac91ec983a7ca1aa7eb7d7769b06570c9a408c',
+  base_url: 'https://ngcentralamerica.com'
+
 
 
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
 end
